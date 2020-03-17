8537f3c15d3bd684a2060ddbed7091b01a76ed4c
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-03-17 11:56:16 +0100

    Added HomeScreen to test localization.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:shopping_cart/localizations.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,13 +8,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Shopping Cart'),
-        ),
-        body: Center(
-          child: Text('Shopping Cart'),
-        ),
+      localizationsDelegates: [
+        const AppLocalizationsDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: AppLocalizationsDelegate.supportedLocals,
+      home: _HomeScreen(),
+    );
+  }
+}
+
+class _HomeScreen extends StatelessWidget {
+  const _HomeScreen({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(AppLocalizations.of(context).appTitle),
       ),
     );
   }
