0e43a24137b0d392fe4e7024ac1f457f0cfa4204
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-03-22 12:01:54 +0100

    Added BottomNavigationBar.

--- lib/widgets/main_screen.dart
+++ lib/widgets/main_screen.dart
@@ -8,9 +8,37 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [];
+  void onTap(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.black,
+        currentIndex: _currentIndex,
+        onTap: onTap,
+        elevation: 0,
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.shopping_cart),
+            title: new Text('Cart'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border),
+            title: Text('Favourite'),
+          ),
+        ],
+      ),
       body: Center(child: Text('Hello')),
     );
   }
