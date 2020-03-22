2b5850aa32c34c0415b2f1f9cf87abd465fd0df2
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-03-22 18:58:05 +0100

    Added getNewestProducts to database service.

--- lib/services/i_database_service.dart
+++ lib/services/i_database_service.dart
@@ -3,4 +3,5 @@ import 'package:shopping_cart/models/product.dart';
 abstract class IDatabaseService {
   Future<void> initialize();
   Future<List<Product>> getAllProducts();
+  Future<List<Product>> getNewestProducts();
 }
