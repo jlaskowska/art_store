83b8c8ac208b114bc82aff4a0042a82438cb7106
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-03-24 10:49:07 +0100

    Updated Teaser.

--- lib/widgets/teaser.dart
+++ lib/widgets/teaser.dart
@@ -2,16 +2,34 @@ import 'package:flutter/material.dart';
 
 class Teaser extends StatelessWidget {
   final String assetPath;
+  final String title;
+  final String author;
 
-  const Teaser({Key key, @required this.assetPath}) : super(key: key);
+  const Teaser({Key key, @required this.assetPath, @required this.title, @required this.author}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(20),
-      child: Image.asset(
-        assetPath,
-        fit: BoxFit.cover,
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.asset(
+            assetPath,
+            fit: BoxFit.cover,
+          ),
+          Text(
+            title,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Text(
+            author,
+            style: TextStyle(fontSize: 12),
+          ),
+        ],
       ),
     );
   }
