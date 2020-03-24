82c83a75310bac5b24374fbae7f872418a6656c8
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-03-24 10:49:22 +0100

    Updated HomeScreen.

--- lib/widgets/home_screen.dart
+++ lib/widgets/home_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+
 import 'package:provider/provider.dart';
-import 'package:shopping_cart/localizations.dart';
+
 import 'package:shopping_cart/models/product.dart';
 
 import 'package:shopping_cart/services/i_database_service.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:shopping_cart/widgets/teaser.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -23,18 +24,14 @@ class _HomeScreenState extends State<HomeScreen> {
 
     if (!hasResolvedDependencies) {
       final sqliteDatabase = Provider.of<IDatabaseService>(context);
-      _productFuture = sqliteDatabase.getNewestProducts();
+      _productFuture = sqliteDatabase.getAllProducts();
 
       hasResolvedDependencies = true;
     }
   }
 
-  int _index = 0;
-
   @override
   Widget build(BuildContext context) {
-    //
-
     return FutureBuilder(
         future: _productFuture,
         builder: (
@@ -42,45 +39,21 @@ class _HomeScreenState extends State<HomeScreen> {
           AsyncSnapshot<List<Product>> snapshot,
         ) {
           if (snapshot.hasData)
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 32.0,
-                    horizontal: 16.0,
-                  ),
-                  child: Text(
-                    AppLocalizations.homeScreenHeadlineNewWork,
-                    style: TextStyle(
-                      fontFamily: GoogleFonts.fingerPaint().fontFamily,
-                      fontSize: 24,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 200,
-                  child: PageView.builder(
-                    itemCount: snapshot.data.length,
-                    controller: PageController(viewportFraction: 0.4),
-                    onPageChanged: (int index) => setState(() => _index = index),
-                    itemBuilder: (_, i) {
-                      return Transform.scale(
-                        scale: i == _index ? 1 : 0.9,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                          child: Teaser(assetPath: snapshot.data[i].assetPath),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ],
+            return StaggeredGridView.countBuilder(
+              crossAxisCount: 2,
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, int index) => Teaser(
+                title: snapshot.data[index].name,
+                author: snapshot.data[index].author,
+                assetPath: snapshot.data[index].assetPath,
+              ),
+              staggeredTileBuilder: (int index) => StaggeredTile.fit(1),
+              mainAxisSpacing: 4.0,
+              crossAxisSpacing: 4.0,
             );
           else if (snapshot.hasError) {}
 
-          return Container();
+          return CircularProgressIndicator();
         });
   }
 }
