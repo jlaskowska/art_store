4f956604d12a0ab9b886f7414e0f41c468ce218b
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-03-24 11:18:12 +0100

    Adding GestureDetector to Teaser.

--- lib/widgets/teaser.dart
+++ lib/widgets/teaser.dart
@@ -4,32 +4,43 @@ class Teaser extends StatelessWidget {
   final String assetPath;
   final String title;
   final String author;
+  final Function onTap;
 
-  const Teaser({Key key, @required this.assetPath, @required this.title, @required this.author}) : super(key: key);
+  const Teaser({Key key, @required this.assetPath, @required this.title, @required this.author, @required this.onTap})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Image.asset(
-            assetPath,
-            fit: BoxFit.cover,
-          ),
-          Text(
-            title,
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          Text(
-            author,
-            style: TextStyle(fontSize: 12),
-          ),
-        ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              assetPath,
+              fit: BoxFit.cover,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0, bottom: 4),
+              child: Text(
+                title,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: Text(
+                author,
+                style: TextStyle(fontSize: 12),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
