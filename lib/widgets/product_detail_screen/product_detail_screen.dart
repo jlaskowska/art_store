65dcd6c64bcccbae3bd9fc2875b2864f8c125337
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-03-26 11:43:43 +0100

    Created ProductDetailScreen.

--- lib/widgets/product_detail_screen/product_detail_screen.dart
+++ lib/widgets/product_detail_screen/product_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_cart/localizations.dart';
 import 'package:shopping_cart/models/product.dart';
 
 class ProductDetailScreen extends StatelessWidget {
@@ -11,9 +12,20 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.favorite_border,
+            ),
+            onPressed: () {},
+          )
+        ],
+      ),
       body: SafeArea(
         child: LayoutBuilder(
           builder: (context, constraints) => Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Container(
                 child: Hero(
@@ -26,11 +38,66 @@ class ProductDetailScreen extends StatelessWidget {
                       product.assetPath,
                       fit: BoxFit.cover,
                       width: constraints.maxWidth,
-                      height: constraints.maxHeight * 0.5,
+                      height: constraints.maxHeight * 0.6,
                     ),
                   ),
                 ),
               ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(left: 32.0, top: 32, bottom: 16),
+                    child: Text(
+                      product.name,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 32,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 32.0, top: 32),
+                    child: Text(
+                      product.price.toString(),
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                child: Text(
+                  product.author,
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+              // Column(
+              //   crossAxisAlignment: CrossAxisAlignment.stretch,
+              //   children: <Widget>[
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    width: constraints.maxWidth * 0.75,
+                    child: ButtonTheme(
+                      height: 50,
+                      child: RaisedButton(
+                        elevation: 0,
+                        color: Colors.black,
+                        child: Text(
+                          AppLocalizations.productDetailScreenAddToBasketButton.toUpperCase(),
+                          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              // ],
+              // ),
             ],
           ),
         ),
