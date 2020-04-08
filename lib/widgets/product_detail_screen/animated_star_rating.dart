3b3aecb626a895ff82b55060e3759d8f9a173e56
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-04-08 14:27:24 +0200

    Added animation to StarRating.

---
+++ lib/widgets/product_detail_screen/animated_star_rating.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import 'star_rating.dart';
+
+class AnimatedStarRating extends StatefulWidget {
+  final double prodcutRating;
+  AnimatedStarRating({
+    Key key,
+    @required this.prodcutRating,
+  }) : super(key: key);
+
+  @override
+  _AnimatedStarRatingState createState() => _AnimatedStarRatingState();
+}
+
+class _AnimatedStarRatingState extends State<AnimatedStarRating> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _controller.duration = Duration(milliseconds: 500);
+
+    _controller.reset();
+    Animation _animation = Tween<double>(begin: 0, end: widget.prodcutRating).animate(_controller);
+    _controller.forward();
+
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (_, __) => StarRating(
+        productRating: _animation.value,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
