b94b655350d9bf4e76183a01286978f83435759a
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-04-11 15:19:19 +0200

    Added error pop up to the HomeScreen.

--- lib/widgets/home_screen.dart
+++ lib/widgets/home_screen.dart
@@ -1,11 +1,11 @@
+import 'package:art_store/localizations.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:provider/provider.dart';
 
 import 'package:art_store/models/product.dart';
-
 import 'package:art_store/services/i_database_service.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:art_store/widgets/common/adaptive_dialog.dart';
 import 'package:art_store/widgets/product_detail_screen/product_detail_screen.dart';
 import 'package:art_store/widgets/teaser.dart';
 
@@ -17,15 +17,17 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  IDatabaseService databaseService;
   Future<List<Product>> _productFuture;
   bool hasResolvedDependencies = false;
+
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
 
     if (!hasResolvedDependencies) {
-      final sqliteDatabase = Provider.of<IDatabaseService>(context);
-      _productFuture = sqliteDatabase.getAllProducts();
+      databaseService = Provider.of<IDatabaseService>(context);
+      _productFuture = databaseService.getAllProducts();
 
       hasResolvedDependencies = true;
     }
@@ -34,33 +36,58 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: _productFuture,
-        builder: (
-          context,
-          AsyncSnapshot<List<Product>> snapshot,
-        ) {
-          if (snapshot.hasData)
-            return StaggeredGridView.countBuilder(
-              crossAxisCount: 2,
-              itemCount: snapshot.data.length,
-              itemBuilder: (BuildContext context, int index) => Teaser(
-                id: snapshot.data[index].id,
-                onTap: () => Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (ctx) => ProductDetailScreen(product: snapshot.data[index]),
-                  ),
+      future: _productFuture,
+      builder: (
+        context,
+        AsyncSnapshot<List<Product>> snapshot,
+      ) {
+        if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
+          return StaggeredGridView.countBuilder(
+            crossAxisCount: 2,
+            itemCount: snapshot.data.length,
+            itemBuilder: (BuildContext context, int index) => Teaser(
+              id: snapshot.data[index].id,
+              onTap: () => Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) => ProductDetailScreen(product: snapshot.data[index]),
                 ),
-                title: snapshot.data[index].name,
-                author: snapshot.data[index].author,
-                assetPath: snapshot.data[index].assetPath,
               ),
-              staggeredTileBuilder: (int index) => StaggeredTile.fit(1),
-              mainAxisSpacing: 4.0,
-              crossAxisSpacing: 4.0,
-            );
-          else if (snapshot.hasError) {}
+              title: snapshot.data[index].name,
+              author: snapshot.data[index].author,
+              assetPath: snapshot.data[index].assetPath,
+            ),
+            staggeredTileBuilder: (int index) => StaggeredTile.fit(1),
+            mainAxisSpacing: 4.0,
+            crossAxisSpacing: 4.0,
+          );
+        } else if (snapshot.connectionState == ConnectionState.done && (snapshot.hasError || snapshot.data == null)) {
+          _showErrorPopup();
+          return Container();
+        }
 
-          return CircularProgressIndicator();
-        });
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+
+  Future<void> _showErrorPopup() async {
+    await Future.delayed(Duration(milliseconds: 1));
+
+    showAdaptiveDialog(
+      context: context,
+      adaptiveDialog: AdaptiveDialog(
+        title: AppLocalizations.adaptiveDialogErrorPopupTitle,
+        content: AppLocalizations.adaptiveDialogErrorPopupContent,
+        actionLabel: AppLocalizations.adaptiveDialogErrorPopupButtonLabel,
+        onPressed: () {
+          Navigator.of(context).pop();
+          setState(() {
+            _productFuture = databaseService.getAllProducts();
+          });
+        },
+      ),
+    );
   }
 }
